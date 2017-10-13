9f6a8e8eb724530bed844a6a21fba10350178110
Silvio Cedrim <engenheirocedrim@gmail.com>
2017-10-12 22:59:19 -0300

    Listagem de Usuários.

--- maanaim_register/pages/usuarios_sistema/lista.php
+++ maanaim_register/pages/usuarios_sistema/lista.php
@@ -5,6 +5,7 @@ require_once ('../../biblioteca/util/Mensagens.php');
 require_once ('../include/header.php');
 require_once ('../menu/menu.php');
 
+$dados = buscarTodosOsRegistros(USUARIO);
 ?>
 
 <!DOCTYPE html>
@@ -26,28 +27,47 @@ require_once ('../menu/menu.php');
         	<div class="row">
         		<div class="panel panel-default">
             		<div class="panel-heading">Lista de Usu&aacute;rios</div>
-            		<div class="panel-body">
-            			<table class="table table-hover">
-            				<thead>
-            					<tr>
-            						<th class="ui-state-default">
-            							<span class="ui-column-title">Nome</span>
-            						</th>
-            						<th class="ui-state-default">
-            							<span class="ui-column-title">E-mail</span>
-            						</th>
-            						<th class="ui-state-default">
-            							<span class="ui-column-title">Login</span>
-            						</th>
-            						<th class="ui-state-default">
-            							<span class="ui-column-title">Administrador</span>
-            						</th>
-            						<th class="ui-state-default">
-            							<span class="ui-column-title">A&ccedil;&otilde;es</span>
-            						</th>
-            					</tr>
-            				</thead>
-            			</table>
+            			<div class="panel-body">
+
+                			<!-- TABLE -->
+                			<table class="table table-bordered">
+                				<thead>
+                					<tr role="row">
+                						<th role="columnheader" class="ui-state-default">
+                							<span class="ui-column-title">Nome</span>
+                						</th>
+                						<th role="columnheader" class="ui-state-default">
+                							<span class="ui-column-title">E-mail</span>
+                						</th>
+                						<th role="columnheader" class="ui-state-default">
+                							<span class="ui-column-title">Login</span>
+                						</th>
+                						<th role="columnheader" class="ui-state-default">
+                							<span class="ui-column-title">Administrador</span>
+                						</th>
+                						<th role="columnheader" class="ui-state-default">
+                							<span class="ui-column-title">A&ccedil;&otilde;es</span>
+                						</th>
+                					</tr>
+                				</thead>
+                				
+                				<?php foreach ($dados as $usuario){ 
+                				?>
+                				
+                				<tbody>
+                					<tr>
+                						<td><?php echo $usuario['nome']; ?></td>
+                						<td><?php echo $usuario['email']; ?></td>
+                						<td><?php echo $usuario['login']; ?></td>
+                						<td><?php echo $usuario['administrador']; ?></td>
+                						<td></td>
+                					</tr>
+                				</tbody>
+                				
+                				<?php }?>
+                				
+                			</table>
+                    		<!-- END TABLE -->
             		</div>
         		</div>
     		</div>
